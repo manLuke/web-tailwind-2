3eb100b9538524899cda4f95eb293d646d0a082e
Tomáš Kácha <kachatomas@gmail.com>
2024-06-03 22:31:53 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7A9D8C",
+        gradientStart: "#ABA8BF",
+        gradientEnd: "#F5E9F3",
+      },
+    },
+  },
+  plugins: [],
+};
